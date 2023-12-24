43958ca6f3eca57286041619f25f7772a6c217ef
Varunbingi <bingivarun27@gmail.com>
2023-12-24 22:00:11 +0530

    second commit

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const parent=React.createElement('div',{id:"parent"},[
-    React.createElement('h1',{id:'heading1'},'Inside h1'),
-    React.createElement('h2',{id:"heading2"},'Inside h2')
-]);
 const root=ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(parent);
+
+
+
+const jsxHeading=<h1 id='heading'>Hello jsx</h1>;
+console.log(jsxHeading)
+
+root.render(jsxHeading);
